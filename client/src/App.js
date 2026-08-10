82eac5d288beea724ab0d95a0e16647f11649edd
Lara Klopper <laraK@corprecover.co.za>
2026-08-10 16:00:42 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 // IMPORT BOOTSTRAP COMPONENTS
 import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
+// IMPORT REACT ROUTER COMPONENTS
+import { Routes } from 'react-router-dom';
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [error, setError] = useState(null)
+  
   return (
     <>
       <Container>
-        
+        <Routes>
+          
+        </Routes>
       </Container>
     </>
   )
