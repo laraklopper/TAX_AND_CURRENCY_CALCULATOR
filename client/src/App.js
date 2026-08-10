587e4bc65df8bda694c590c2b6d77ea1eb532d59
Lara Klopper <laraK@corprecover.co.za>
2026-08-10 15:57:14 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css'
-
+// IMPORT BOOTSTRAP COMPONENTS
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 export default function App() {
   return (
     <>
-      
+      <Container>
+        
+      </Container>
     </>
   )
 }
