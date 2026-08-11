3846d6daab79ed87a9e263b5fa3d44e08f288971
Lara Klopper <laraK@corprecover.co.za>
2026-08-11 10:16:13 +0200

    schema

--- server/models/curConvertSchema.js
+++ server/models/curConvertSchema.js
@@ -1,5 +1,35 @@
 const mongoose = require('mongoose');
 
 const currencyConvertSchema = new mongoose.Schema({
-    
+    // Field for current user fullName
+    fullName : {
+        firstName: {
+            
+
+        },
+        lastName:{
+
+        },
+    },
+    currency: {
+        baseCurrency: {
+            type: String,
+
+        },
+        targetCurrency:{
+            
+
+        },
+    },
+    amount: {
+
+    },
+    rate:{
+
+    }
+},{timestamps:true,
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
 });
+
+module.exports = mongoose.model('currency', currencyConvertSchema);
