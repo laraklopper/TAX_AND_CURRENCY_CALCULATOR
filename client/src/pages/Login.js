14f06a76dfd6dd32764f9ded1c239fbfcdc60a83
Lara Klopper <laraK@corprecover.co.za>
2026-08-11 15:00:16 +0200

    changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import MainHeader from '../components/MainHeader'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <MainHeader/>
+    </div>
   )
 }
