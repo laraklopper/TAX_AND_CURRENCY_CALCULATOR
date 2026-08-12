27d25f9383f68abfc4b04efc36e9b805371e5625
Lara Klopper <laraK@corprecover.co.za>
2026-08-12 16:32:40 +0200

    changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,10 @@
 import React, { useCallback } from 'react'
 import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/LoggedOut.css'
+import '../css/componentCss/Footer.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import Stack from 'react-bootstrap/Stack';
 import MainHeader from '../components/MainHeader'
 import LoginForm from '../components/LoginForm';
 
@@ -64,6 +65,27 @@ export default function Login({userData, setUserData, setError, loggedIn, setLog
         <Col id='login-col2'/>
       </Row>
       </section>
+         <footer className='pageFooter'>
+        <Row id='footerRow1'>
+          <Col id='footer-col1'>
+            <Stack direction="horizontal" gap={3} id='page-footer-stack'>
+      <div className="p-2"></div>
+      <div className="p-2 ms-auto"></div>
+      <div className="p-2"></div>
+    </Stack>
+          </Col>
+        </Row>
+         {/* Row 3: CopyRight Information */}
+      <Row id='copyRightRow'>
+        <Col xs={0} md id='copyRightCol1'/>
+        <Col xs={12} md={4} id='copyRightCol'>
+          <div id='copyright-div' >
+            <p id='copyrightText' aria-label='copy right details' aria-live='polite' >© 2026 Tax Calculator App. All rights reserved.</p>
+          </div>
+        </Col>
+        <Col xs={0} md id='copyRightCol2'/>
+      </Row>
+      </footer>
     </div>
   )
 }
--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,8 +1,11 @@
 import React, { useCallback, useState } from 'react'
 import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/LoggedOut.css'
+import '../css/componentCss/Footer.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+
 import MainHeader from '../components/MainHeader'
 import RegistrationForm from '../components/RegistrationForm';
 import { useNavigate } from 'react-router-dom';
@@ -75,8 +78,24 @@ export default function Registration({setError}) {
       </section>
       <footer className='pageFooter'>
         <Row id='footerRow1'>
-          <Col></Col>
+          <Col id='footer-col1'>
+            <Stack direction="horizontal" gap={3} id='page-footer-stack'>
+      <div className="p-2"></div>
+      <div className="p-2 ms-auto"></div>
+      <div className="p-2"></div>
+    </Stack>
+          </Col>
         </Row>
+         {/* Row 3: CopyRight Information */}
+      <Row id='copyRightRow'>
+        <Col xs={0} md id='copyRightCol1'/>
+        <Col xs={12} md={4} id='copyRightCol'>
+          <div id='copyright-div' >
+            <p id='copyrightText' aria-label='copy right details' aria-live='polite' >© 2026 Tax Calculator App. All rights reserved.</p>
+          </div>
+        </Col>
+        <Col xs={0} md id='copyRightCol2'/>
+      </Row>
       </footer>
     </div>
   )
