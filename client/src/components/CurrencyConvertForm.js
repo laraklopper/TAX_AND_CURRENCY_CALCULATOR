5b9e198e499bdc60da5d66bba2f3cfc75d321799
Lara Klopper <laraK@corprecover.co.za>
2026-08-13 13:16:26 +0200

    changes

---
+++ client/src/components/CurrencyConvertForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/componentCss/FormSetup.css'
+
+export default function CurrencyConvertForm() {
+  return (
+    <div>CurrencyConvertForm</div>
+  )
+}
--- client/src/pages/CurrencyConverter.js
+++ client/src/pages/CurrencyConverter.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/CurrencyConvert.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -9,7 +11,17 @@ export default function CurrencyConverter({currentUser, logout}) {
     <div id='pageContainer' role='main'>
       <Header currentUser={currentUser} pageHeader={'CURRENCY CONVERTER'}/>
       <section id='currency-converter-sec1'>
-        {/*Currency Converter form  */}
+        
+        <Row>
+        <Col/>
+        <Col xs={6}>
+          <div id='currency-converter-panal'>
+{/*Currency Converter form  */}
+          </div>
+        </Col>
+        <Col/>
+      </Row>
+
       </section>
       <Footer logout={logout}/>
     </div>
