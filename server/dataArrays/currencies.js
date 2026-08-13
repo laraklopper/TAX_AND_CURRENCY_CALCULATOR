93eb0322924a64659d8ecd1dc75be34a03a5d970
Lara Klopper <laraK@corprecover.co.za>
2026-08-13 14:45:14 +0200

    changes

---
+++ server/dataArrays/currencies.js
@@ -0,0 +1,6 @@
+export const currencies = [
+    'AED', 'AUD', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EGP',
+    'EUR', 'GBP', 'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW',
+    'MXN', 'MYR', 'NGN', 'NOK', 'NZD', 'PHP', 'PKR', 'PLN', 'SAR',
+    'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'UAH', 'USD', 'ZAR',
+];
