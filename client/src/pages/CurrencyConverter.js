f7d4902495882eadf71ab5372ca75a7975db8547
Lara Klopper <laraK@corprecover.co.za>
2026-08-13 13:14:17 +0200

    changes

--- client/src/pages/CurrencyConverter.js
+++ client/src/pages/CurrencyConverter.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../css/pagesCss/PageSetup.css'
+import '../css/pagesCss/CurrencyConvert.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function CurrencyConverter({currentUser, logout}) {
   return (
-    <div id='pageContainer'>
+    <div id='pageContainer' role='main'>
       <Header currentUser={currentUser} pageHeader={'CURRENCY CONVERTER'}/>
+      <section id='currency-converter-sec1'>
+        {/*Currency Converter form  */}
+      </section>
       <Footer logout={logout}/>
     </div>
   )
