534fa7ddd893102e2d6419bd74fe30786e858825
Lara Klopper <laraK@corprecover.co.za>
2026-08-13 08:34:32 +0200

    changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Header from '../components/Header'
 export default function Dashboard({currentUser}) {
   return (
-    <div>
+    <div id='pageContainer' role='main'>
       <Header currentUser={currentUser}/>
+      <section>
+        
+      </section>
     </div>
   )
 }
