618c190aed952c23db694e9e7ab9a618462b3624
Lara Klopper <laraK@corprecover.co.za>
2026-08-15 11:59:49 +0200

    comments

---
+++ client/src/components/CurrencyList.js
@@ -0,0 +1,11 @@
+// CurrencyList.js
+import React from 'react'
+// display all available currencies in the data array in table format
+// add the country/s where the currency is used
+export default function CurrencyList() {
+  return (
+    <div>
+        {/* Currency list table */}
+    </div>
+  )
+}
