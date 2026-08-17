e1577eb31740d987e06b49325b4342a8cb5e0917
Lara Klopper <laraK@corprecover.co.za>
2026-08-17 09:32:01 +0200

    locationarray

---
+++ client/src/dataArrays/locations.js
@@ -0,0 +1,11 @@
+export const southAfricanProvinces = [
+  "Eastern Cape",
+  "Free State",
+  "Gauteng",
+  "KwaZulu-Natal",
+  "Limpopo",
+  "Mpumalanga",
+  "North West",
+  "Northern Cape",
+  "Western Cape",
+];
