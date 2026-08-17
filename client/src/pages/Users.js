fc4e1c5fb4824298409a8912330a1713e64c0902
Lara Klopper <laraK@corprecover.co.za>
2026-08-17 13:28:38 +0200

    changes

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/Users.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
@@ -8,6 +11,21 @@ export default function Users({currentUser, logout}) {
   return (
     <div id='pageContainer'>
       <Header currentUser={currentUser} pageHeader={'ADMIN'}/>
+        <section id='admin-section1'>
+          {/* EDIT TAX DATA FORM */}
+          <Row id='toggle-tax-update-col'>
+        <Col/>
+        <Col xs={6} id='toggle-tax-formCol'>
+          <div className='toggle-div'>
+            <Button variant='warning'>
+              UPDATE TAX DATA
+            </Button>
+          </div>
+        </Col>
+        <Col/>
+      </Row>
+
+        </section>
       <Footer logout={logout}/>
     </div>
   )
