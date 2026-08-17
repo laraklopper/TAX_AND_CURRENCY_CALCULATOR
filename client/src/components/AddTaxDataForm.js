2942bd18bb700ac697da8a7bb4a8280f6b7c649c
Lara Klopper <laraK@corprecover.co.za>
2026-08-17 13:45:08 +0200

    changes

--- client/src/components/AddTaxDataForm.js
+++ client/src/components/AddTaxDataForm.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import '../css/componentCss/TaxForm.css'
+import '../css/componentCss/FormSetup.css'
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+
 
 export default function AddTaxDataForm() {
   return (
--- client/src/pages/TaxData.js
+++ client/src/pages/TaxData.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-export default function Users({currentUser, logout}) {
+export default function TaxData({currentUser, logout}) {
   return (
     <div id='pageContainer'>
       <Header currentUser={currentUser} pageHeader={'ADMIN'}/>
