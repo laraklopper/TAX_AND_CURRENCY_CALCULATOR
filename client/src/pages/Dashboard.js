16c6630bdcc75d9984c609ca326326bc96d1e8ad
Lara Klopper <laraK@corprecover.co.za>
2026-08-18 12:03:00 +0200

    changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,14 +3,19 @@ import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/Dashboard.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 
 export default function Dashboard({currentUser, logout}) {
   const [key, setKey] = useState('home');
+  const [showChangeTaxYear, setShowChangeTaxYear] =useState(false)
+
+  const toggleChangeTaxYear = () => setShowChangeTaxYear((prev) => !prev)
   return (
     <div id='pageContainer' role='main'>
       <Header currentUser={currentUser} pageHeader={'DASHBOARD'}/>
@@ -29,13 +34,58 @@ export default function Dashboard({currentUser, logout}) {
               Tab content for Profile
             </Tab>
             <Tab eventKey="taxRef" title="TAX YEAR REFERENCE">
-                <Row id='tax-year-head-row'>
-        <Col id='taxYearHeadCol1'>1 of 3</Col>
+            <div>
+         <Row id='tax-year-head-row'>
+        <Col id='taxYearHeadCol1'/>
         <Col xs={6} id='taxYearHeadCol'>
-          <span><h4>TAX YEAR 2025/2026 Tax Year</h4></span>
+          <span id='tax-year-span'><h4 id='tax-year-head'>TAX YEAR:</h4><h4 id='current-tax-year'> 2025/2026</h4></span>
         </Col>
-        <Col id='taxYearHeadCol2'>3 of 3</Col>
+        <Col id='taxYearHeadCol2'>
+         
+          
+        </Col>
       </Row>
+      <Row>
+        <Col>
+          {/* DISPLAY CURRENT TAX YEAR DATA FROM FORM */}
+        </Col>
+      </Row>
+<Row id='change-tax-year-row'>
+        <Col id='change-tax-year-col1'></Col>
+        <Col xs={6} id='change-tax-year-toggle'>
+           <div id='toggle-tax-year-div'>
+            <Button variant='warning' id='toggle-tax-year-btn' onClick={toggleChangeTaxYear}>CHANGE TAX YEAR</Button>
+          {showChangeTaxYear && (
+            <div id='change-tax-year-panal'>
+              <form id='change-tax-year-form'>
+                <div>
+                  <Stack gap={3}>
+                  <div className="p-2" id='edit-year-block'>
+                    <label className='edit-taxyear-label'>TAX YEAR</label>
+                    <input
+                      id='edit-taxyear-input'
+                    />
+                  </div>
+                  <div className="p-2">
+                    <Button
+                    variant='warning'
+                    >CHANGE YEAR</Button>
+                  </div>
+                 
+              </Stack>
+                </div>
+
+
+              </form>
+            </div>
+          )}
+          </div>
+          
+        </Col>
+        <Col></Col>
+      </Row>
+            </div>
+       
 
             </Tab>
           </Tabs>
