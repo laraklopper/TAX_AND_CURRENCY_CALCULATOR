219f54be68e45e2085117693dba4d74efeea2445
Lara Klopper <laraK@corprecover.co.za>
2026-08-18 10:34:29 +0200

    changes

--- client/src/pages/TaxData.js
+++ client/src/pages/TaxData.js
@@ -17,18 +17,29 @@ export default function TaxData({currentUser, logout}) {
     <div id='pageContainer'>
       <Header currentUser={currentUser} pageHeader={'ADMIN'}/>
         <section id='admin-section1'>
-          {/* ADD/EDIT TAX DATA FORM */}
+          {/* Section 1 row 1 */}
           <Row id='toggle-tax-update-row'>
         <Col/>
         <Col xs={6} id='toggle-tax-formCol'>
           <div className='toggle-div'>
-            <Button variant='warning' onClick={toggleTaxDataForm}>
-              ADD/UPDATE TAX DATA
+            <Button 
+              variant='warning' 
+              onClick={toggleTaxDataForm} 
+              id='toggleTaxChangeBtn'
+              type='button'
+              // ARIA ATTRIBUTES:
+              aria-label={showTaxForm ? 'HIDE TAX FORM': 'ADD/UPDATE TAX DATA'}
+              aria-controls='change-tax-data-panal'
+              aria-pressed={showTaxForm}
+              aria-expanded={showTaxForm}
+              >
+              {showTaxForm ? 'HIDE TAX FORM': 'ADD/UPDATE TAX DATA'}
             </Button>
           </div>
         </Col>
         <Col/>
       </Row>
+      {/*TOGGLE ADD/EDIT TAX DATA FORM */}
       {showTaxForm && (
           <div id='change-tax-data-panal'>
              <Row id='add-tax-data-row'>
