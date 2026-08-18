34cb7ba3002af6545c1a396d5e3ce3bfeb8b0987
Lara Klopper <laraK@corprecover.co.za>
2026-08-18 13:41:25 +0200

    changes

---
+++ client/src/components/TaxDataDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../css/componentCss/TaxDataDisplay.css'
+export default function TaxDataDisplay() {
+  return (
+    <div>
+        {/* Display seed data */}
+    </div>
+  )
+}
