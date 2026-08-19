a3c0688056b697f4c74ae5822c589d7c4488b908
Lara Klopper <laraK@corprecover.co.za>
2026-08-19 12:34:24 +0200

    new calculator form

---
+++ client/src/components/InterestCalculatorForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/componentCss/FormSetup.css'
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+
+export default function InterestCalculatorForm() {
+  return (
+    <div>InterestCalculatorForm</div>
+  )
+}
