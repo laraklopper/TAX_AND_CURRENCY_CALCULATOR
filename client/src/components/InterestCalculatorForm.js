cd469d0ceded3ef494043dab96c1100cbc1927e0
Lara Klopper <laraK@corprecover.co.za>
2026-08-19 12:38:31 +0200

    changes

--- client/src/components/InterestCalculatorForm.js
+++ client/src/components/InterestCalculatorForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../css/componentCss/FormSetup.css'
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
+import { Calculator, RotateCcw, TrendingUp, AlertCircle } from "lucide-react";
 
 export default function InterestCalculatorForm() {
   return (
