189dc6f6f2203d3828e50acb1f81b202e246c59e
Lara Klopper <laraK@corprecover.co.za>
2026-08-21 08:51:25 +0200

    middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,24 @@
+//userRoutes.js
+/* Load environment variables from a .env 
+file using the dotenv package*/
+require('dotenv').config();
 const express = require('express');
 const router = express.Router()
 
+const User = require('../models/userSchema')
+
+// ======ROUTES=====================
+/*──────────────────────────── GET ROUTES ─────────────────────────────────────
+   GET: READ — Used to fetch information from the database
+────────────────────────────────────────────────────────────────────────────────*/
+
+/*──────────────────────────── PATCH ROUTES ───────────────────────────────────
+   PATCH: UPDATE — Used to partially update information in the database
+────────────────────────────────────────────────────────────────────────────────*/
+
+/*──────────────────────────── DELETE ROUTES ───────────────────────────────────
+    DELETE: Used to remove an item from the database
+ ────────────────────────────────────────────────────────────────────────────────*/
+ //Route to send a DELETE request to the /deleteUser/:id endpoint
+// =====================
 module.exports = router
