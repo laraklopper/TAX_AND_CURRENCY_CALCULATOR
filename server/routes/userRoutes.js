c6640cbc705a13e16ece161e286f15a992a853cc
Lara Klopper <laraK@corprecover.co.za>
2026-08-21 10:24:00 +0200

    changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,6 +12,7 @@ const { checkJwtToken } = require('./middleware');
 /*──────────────────────────── GET ROUTES ─────────────────────────────────────
    GET: READ — Used to fetch information from the database
 ────────────────────────────────────────────────────────────────────────────────*/
+// Send a get request to /users/fetchUsers endpoint to fetch all users
 router.get('/fetchUsers', checkJwtToken, async (req, res) => {
     try {
         const users = await User.find()
@@ -28,6 +29,29 @@ router.get('/fetchUsers', checkJwtToken, async (req, res) => {
     }
     
 })
+// send a GET request to /users/me endpoint to fetch current user data
+router.get('/me', checkJwtToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId)
+        .select('-password')
+        .exec();
+
+        //Conditional rendering to check if user is found
+        if (!user) {
+            console.error('User not found with ID:', userId);
+            return res.status(404).json({ error: 'User not found' }); 
+        }
+
+        console.log(`[RESPONSE: userRoutes]`, user);
+        return res.status(200).json(user); 
+    } catch (error) {
+        console.error('Error fetching current user:', error);
+        res.status(500).json({
+            success: false, 
+            error: 'An error occurred while fetching the current user'});
+    }
+})
 /*──────────────────────────── PATCH ROUTES ───────────────────────────────────
    PATCH: UPDATE — Used to partially update information in the database
 ────────────────────────────────────────────────────────────────────────────────*/
@@ -43,5 +67,28 @@ router.patch('/editPassword', async (req, res) => {
     DELETE: Used to remove an item from the database
  ────────────────────────────────────────────────────────────────────────────────*/
  //Route to send a DELETE request to the /deleteUser/:id endpoint
+ router.delete('/deleteUser/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const removedUser = await User.findByIdAndDelete(id);
+
+        if (!removedUser) {
+            console.error('[ERROR: userRoutes.js, /deleteUser/:id] : User not found');
+             return res.status(404).json({//Send a 404(Not Found) status response with an error message in JSON response
+                success: false, 
+                message: 'User not found' //JSON message
+            });
+        }
+
+        console.log(`[SUCCESS: userRoutes.js, DELETE /deleteUser/:id] Deleted user: ${id}`);
+        return res.status(200).json({
+             success: true, 
+             message: 'User deleted successfully.' 
+            });
+    } catch (error) {
+        console.error('Error deleting user:', error.message);
+        res.status(500).json({ error: 'Failed to delete User' });
+    }
+ })
 // =====================
 module.exports = router
