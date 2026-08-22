89b7b40f67002eba6d82d9b5873450b8450c6627
Lara Klopper <laraK@corprecover.co.za>
2026-08-22 11:51:23 +0200

    pageSetup

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,8 +1,29 @@
+// Users.js
+//IMPORT REQUIRED MODULES AND PACKAGES
 import React from 'react'
+// IMPORT CSS STYLESHEETS
 import '../css/pagesCss/PageSetup.css'
 import '../css/pagesCss/Users.css'
-export default function Users() {
+//IMPORT CUSTOM COMPONENTS
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+// =======MAIN USERS PAGE COMPONENT===========
+export default function Users(
+  {//PROPS PASSED FROM PARENT COMPONENT (App.js)
+    currentUser, 
+    logout
+  }) {
+
+    //============JSX RENDERING=============
   return (
-    <div>Users</div>
+    <div>
+      <Header pageHeader='USERS' currentUser={currentUser}/>
+      <section>
+
+      </section>
+      <Footer logout={logout}/>
+
+    </div>
   )
 }
