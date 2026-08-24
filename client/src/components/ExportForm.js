3910d34042c49031bbc531e184d6e7392505758a
Lara Klopper <laraK@corprecover.co.za>
2026-08-24 15:15:02 +0200

    changes

--- client/src/components/ExportForm.js
+++ client/src/components/ExportForm.js
@@ -1,9 +1,11 @@
 // ExportForm.js
 import React, { useState } from 'react'
+import '../css/componentCss/ExportForm.css'
 import '../css/componentCss/FormSetup.css'
 // IMPORT BOOTSTRAP COMPONENTS
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button'
+
 
 // Export form to export currency, tax and interest calculations
 // Use single form for all exports
@@ -12,7 +14,8 @@ export default function ExportForm() {
     const [error, setError] = useState(null)// State to store any error messages that should be displayed to the user.
    // Tracks whether the download request is currently running.
     const [loading, setLoading] = useState(false)// State Used to disable the button and show loading text.
-  return (
+
+    return (
     <form aria-labelledby='formTitle'>
         <p className='visually-hidden' id='formTitle'>
             {/* EXPORT TITLE: e.g Export Currency calculations */}
