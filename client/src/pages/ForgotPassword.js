d1e82dae6a140c5f0f27a09678ad8fd4ee4cba80
Lara Klopper <laraK@corprecover.co.za>
2026-08-25 09:09:22 +0200

    page imports

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,8 +1,16 @@
 // ForgotPassword.js
 import React from 'react'
-import '.../'
-export default function ForgotPassword() {
+import '../css/pagesCss/ForgotPassword.css'
+import '../css/pagesCss/PageSetup.css'
+import '../css/componentCss/FormSetup.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+
+// ============MAIN FORGOT PASSWORD COMPONENT============
+export default function ForgotPassword() {//Export default ForgotPassword.js component
   return (
-    <div>ForgotPassword</div>
+    <div id='pageContainer'>ForgotPassword</div>
   )
 }
