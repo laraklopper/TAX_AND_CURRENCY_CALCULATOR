abad017ab622137e24d44ce16f1b01a0c6de91c9
Lara Klopper <laraK@corprecover.co.za>
2026-08-25 12:20:04 +0200

    changes

--- client/src/pages/PasswordReset.js
+++ client/src/pages/PasswordReset.js
@@ -15,6 +15,7 @@ import Button from 'react-bootstrap/Button'
 import { useParams, Link } from 'react-router-dom'
 //IMPORT CUSTOM COMPONENTS
 import PageFooter from '../components/PageFooter'
+import ResetPswdHeader from '../components/ResetPswdHeader'
 
 //ResetPassword Function component
 export default function PasswordReset() {
@@ -70,143 +71,129 @@ export default function PasswordReset() {
 
     //================JSX RENDERING==================
   return (
-    <div aria-labelledby='pageTitle'>
+    <div id='pageContainer' aria-labelledby='pageTitle'>
         {/* -------Screen Reader heading------------ */}
             <p className='visually-hidden' id='pageTitle'>RESET PASSWORD PAGE</p>
-             {/* HEADER */}
-        <header id='forgotPswdHeader' role='banner'>
-            <Row id='mainHeaderCol1' md={12}>
-                    <Col id='forgotPswdHeadCol1'/>
-            </Row>
-             <Row id='headerRow2'>
-        <Col/>
-        <Col xs={5} id='app-heading-col'>
-            <span className='headerSpan'>
-                <h1 id='appHeading'>TAX, CURRENCY & INTEREST CALCULATOR</h1>
-                <h2 id='page-heading'>RESET PASSWORD</h2>
-            </span>
-        </Col>
-        <Col/>
-      </Row>
-      <Row id='forgotPswdHeadRow3'>
-            <Col id='forgotPswdHeadCol3'/>
-        </Row>
-        </header>
-        <section>
-            <Col xs={0} md id='forgot-password-col1' />
-            <Col xs={12} md={6} id='forgot-password-col'>
-                <div id='reset-password-panal'>
-                    <form id='reset-password-form' onSubmit={handleSubmit} aria-labelledby='formTitle'>
-                    {/* ---------Screen Reader Heading--------- */}
-                        <p className='visually-hidden' id='formTitle'>RESET PASSWORD FORM</p>
-                        <div id='reset-password-details'>
-                            <Stack gap={3} id='reset-pswd-stack1'>
-                                <div className="p-2">
-                                     <p id='forgotPswdHeading'>
-                                        Enter your new password below.
-                                    </p>
-                                </div>
-                                {successMsg &&(
+              {/* =========HEADER========= */}
+    {/* Render the ResetPswdHeader.js function component 
+    with 'RESET PASSWORD' as the resetPswdHeading */}
+        <ResetPswdHeader resetPswdHeading='RESET PASSWORD'/>
+        <section id='reset-password-section'>
+            <Row id='reset-password-row'>
+                <Col xs={0}  id='forgot-password-col1' />
+                <Col xs={12} md={6} id='forgot-password-col'>
+                    <div id='reset-password-panal'>
+                        <form id='reset-password-form' onSubmit={handleSubmit} aria-labelledby='formTitle'>
+                        {/* ---------Screen Reader Heading--------- */}
+                            <p className='visually-hidden' id='formTitle'>RESET PASSWORD FORM</p>
+                            <div id='reset-password-details'>
+                                <Stack gap={3} id='reset-pswd-stack1'>
                                     <div className="p-2">
-                                        <p id='resetSuccessMsg'>{successMsg}</p>
+                                        <p id='forgotPswdHeading'>
+                                            Enter your new password below.
+                                        </p>
                                     </div>
-                                )}
-                                 {/* ========ERROR MESSAGE========== */}
-                                {error && (
-                                    <div className='p-2' role='alert' aria-live='assertive'>
-                                        <p id='resetErrorMsg'>{error}</p>
-                                    </div>
-                                )}
-                                {!successMsg && (
-                                    <>
-                                        <div className="p-2" id='newPswdInputBlock'>
-                                            <label
-                                                className='resetPswdLabel'
-                                                htmlFor='newPswdInput'
-                                                hidden
-                                                >
-                                                NEW PASSWORD
-                                            </label>
-                                            <input
-                                                className='input'
-                                                id='newPswdInput'
-                                                type='password'
-                                                required
-                                                placeholder='NEW PASSWORD'
-                                                autoComplete='new-password'
-                                                value={password}
-                                                disabled={loading}
-                                                onFocus={() => setPasswordMsg(true)}
-                                                onBlur={() => setPasswordMsg(false)}
-                                                onChange={(e) => setPassword(e.target.value)}
-                                                // ARIA ATTRIBUTES
-                                                aria-required='true'
-                                                aria-label='New Password'
-                                                aria-disabled={loading}
-                                            />
+                                    {successMsg &&(
+                                        <div className="p-2">
+                                            <p id='resetSuccessMsg'>{successMsg}</p>
                                         </div>
-                                        <div className="p-2" id='confirmPswdInputBlock'>
-                                            <label
-                                            className='resetPswdLabel'
-                                            htmlFor='confirmPswdInput'
-                                            hidden
-                                            >CONFIRM PASSWORD</label>
-                                            <input
-                                                className='input'
-                                                id='confirmPswdInput'
-                                                type='password'
-                                                required
-                                                placeholder='CONFIRM PASSWORD'
-                                                autoComplete='new-password'
-                                                value={confirmPassword}
-                                                disabled={loading}
-                                                onFocus={() => setPasswordMsg(true)}
-                                                onBlur={() => setPasswordMsg(false)}
-                                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                                // ARIA ATTRIBUTES:
-                                                aria-label='Confirm New Password'
-                                                aria-required='true'
-                                                aria-disabled={loading}
-                                            />
+                                    )}
+                                    {/* ========ERROR MESSAGE========== */}
+                                    {error && (
+                                        <div className='p-2' role='alert' aria-live='assertive'>
+                                            <p id='resetErrorMsg'>{error}</p>
                                         </div>
-                                        {passwordMsg && (
-                                            <div>
-                                                <p className='infoMsg'>WE WILL NEVER SHARE YOUR PASSORD</p>
+                                    )}
+                                    {!successMsg && (
+                                        <>
+                                            <div className="p-2" id='newPswdInputBlock'>
+                                                <label
+                                                    className='resetPswdLabel'
+                                                    htmlFor='newPswdInput'
+                                                    hidden
+                                                    >
+                                                    NEW PASSWORD
+                                                </label>
+                                                <input
+                                                    className='input'
+                                                    id='newPswdInput'
+                                                    type='password'
+                                                    required
+                                                    placeholder='NEW PASSWORD'
+                                                    autoComplete='new-password'
+                                                    value={password}
+                                                    disabled={loading}
+                                                    onFocus={() => setPasswordMsg(true)}
+                                                    onBlur={() => setPasswordMsg(false)}
+                                                    onChange={(e) => setPassword(e.target.value)}
+                                                    // ARIA ATTRIBUTES
+                                                    aria-required='true'
+                                                    aria-label='New Password'
+                                                    aria-disabled={loading}
+                                                />
                                             </div>
-                                        )}
-                                        <div className='p-2' id='resetPswdBtnBlock'>
-                                            <Button
-                                            variant='warning'
-                                            type='submit'
-                                            id='sendResetPswdBtn'
-                                            disabled={loading}
-                                            // ARIA ATTRIBUTES:
-                                            aria-label={loading ? 'RESETTING...' : 'RESET PASSWORD'}
-                                            aria-disabled={loading}
-                                            aria-busy={loading}
-                                            >
-                                             {loading ? 'RESETTING...' : 'RESET PASSWORD'}
-                                            </Button>
-                                        </div>
-                                    </>
-                                )}
-                                <div className='p-2' id='backToLoginBlock'>
-                                    <Link
-                                        id='loginLink'
-                                        to='/'
-                                        aria-label={successMsg ? 'GO TO LOGIN' : 'BACK TO LOGIN'}
-                                    >
-                                        {successMsg ? 'GO TO LOGIN' : 'BACK TO LOGIN'}
-                                    </Link>
+                                            <div className="p-2" id='confirmPswdInputBlock'>
+                                                <label
+                                                className='resetPswdLabel'
+                                                htmlFor='confirmPswdInput'
+                                                hidden
+                                                >CONFIRM PASSWORD</label>
+                                                <input
+                                                    className='input'
+                                                    id='confirmPswdInput'
+                                                    type='password'
+                                                    required
+                                                    placeholder='CONFIRM PASSWORD'
+                                                    autoComplete='new-password'
+                                                    value={confirmPassword}
+                                                    disabled={loading}
+                                                    onFocus={() => setPasswordMsg(true)}
+                                                    onBlur={() => setPasswordMsg(false)}
+                                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                                    // ARIA ATTRIBUTES:
+                                                    aria-label='Confirm New Password'
+                                                    aria-required='true'
+                                                    aria-disabled={loading}
+                                                />
+                                            </div>
+                                            {passwordMsg && (
+                                                <div>
+                                                    <p className='infoMsg'>WE WILL NEVER SHARE YOUR PASSORD</p>
+                                                </div>
+                                            )}
+                                            <div className='p-2' id='resetPswdBtnBlock'>
+                                                <Button
+                                                variant='warning'
+                                                type='submit'
+                                                id='sendResetPswdBtn'
+                                                disabled={loading}
+                                                // ARIA ATTRIBUTES:
+                                                aria-label={loading ? 'RESETTING...' : 'RESET PASSWORD'}
+                                                aria-disabled={loading}
+                                                aria-busy={loading}
+                                                >
+                                                {loading ? 'RESETTING...' : 'RESET PASSWORD'}
+                                                </Button>
+                                            </div>
+                                        </>
+                                    )}
+                                    <div className='p-2' id='backToLoginBlock'>
+                                        <Link
+                                            id='loginLink'
+                                            to='/'
+                                            aria-label={successMsg ? 'GO TO LOGIN' : 'BACK TO LOGIN'}
+                                        >
+                                            {successMsg ? 'GO TO LOGIN' : 'BACK TO LOGIN'}
+                                        </Link>
 
-                                </div>
-                            </Stack>
-                        </div>
-                    </form>
-                </div>
-            </Col>
-            <Col xs={0} md id='forgot-password-col2' />
-
+                                    </div>
+                                </Stack>
+                            </div>
+                        </form>
+                    </div>
+                </Col>
+                <Col xs={0} id='forgot-password-col2' />
+            </Row>
         </section>
         <PageFooter/>
     </div>
