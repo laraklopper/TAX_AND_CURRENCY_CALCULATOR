8fceceedf1412c76651dd664cbf80f3c96e97d81
Lara Klopper <laraK@corprecover.co.za>
2026-08-26 13:28:05 +0200

    changes

---
+++ client/src/components/ProvTaxCalculations.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/componentCss/CalculationsList.css'
+import '../css/componentCss/DetailsPanal.css'
+export default function ProvTaxCalculations() {
+  return (
+    <div>ProvTaxCalculations</div>
+  )
+}
