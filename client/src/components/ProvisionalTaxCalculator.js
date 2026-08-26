b19b18f8769b46f0fa959e26ae5099bc67892090
Lara Klopper <laraK@corprecover.co.za>
2026-08-26 09:30:04 +0200

    changes

---
+++ client/src/components/ProvisionalTaxCalculator.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/componentCss/Calculators.css'
+import '../css/componentCss/CalculatorDisplay.css'
+export default function ProvisionalTaxCalculator() {
+  return (
+    <div>ProvisionalTaxCalculator</div>
+  )
+}
---
+++ client/src/components/VatCalculator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../css/componentCss/Calculators.css'
+import '../css/componentCss/CalculatorDisplay.css'
+
+export default function VatCalculator() {
+  return (
+    <div>VatCalculator</div>
+  )
+}
